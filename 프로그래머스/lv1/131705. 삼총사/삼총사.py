6a2ed272f510a1e9106da7caf78f964be7dadcc2
be4rpooh02 <37266962+be4rpooh02@users.noreply.github.com>
2023-10-04 01:15:35 +0900

    [level 1] Title: 삼총사, Time: 0.05 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.py
@@ -0,0 +1,10 @@
+from itertools import combinations
+
+def solution(number):
+    answer = 0
+    
+    for item in combinations(number, 3):
+        if(not sum(item)):
+            answer+=1
+    
+    return answer
