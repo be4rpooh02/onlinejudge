bee95a133abe4fea89994c4015194cdcb34bcdff
be4rpooh02 <37266962+be4rpooh02@users.noreply.github.com>
2023-10-03 18:07:22 +0900

    Delete programmers directory

--- programmers/모음 제거/solution.py
+++
@@ -1,5 +0,0 @@
-// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/120849
-
-def solution(my_string):
-    answer = ''.join([ch for ch in my_string if(ch not in ['a','e','i','o','u'])])
-    return answer
