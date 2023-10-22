42fba321b9035bf1807c93fbf45e5d83f481ad8d
Sangwoo Shim <37266962+be4rpooh02@users.noreply.github.com>
2023-10-22 22:46:21 +0900

    [level 2] Title: 전화번호 목록, Time: 178.69 ms, Memory: 34.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,19 +1,21 @@
 def solution(phone_book):
     d = {}
+    answer = True
     
     for phone in phone_book:
-        d[phone] = len(phone)
-        
-    answer = True
+        d[phone] = 1
 
     for key in d.keys():
         prefix = ""
         
         for ch in key:
-            prefix+=ch
-            
+            prefix += ch 
+
             if prefix in d and prefix != key:
                 answer = False
                 break
-
+                
+        if not answer:
+            break
+        
     return answer
