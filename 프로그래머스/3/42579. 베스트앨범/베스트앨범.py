b5086da92b2e85fc52224d6188ad086f1c588b72
Sangwoo Shim <37266962+be4rpooh02@users.noreply.github.com>
2023-10-23 15:32:40 +0900

    [level 3] Title: 베스트앨범, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -0,0 +1,25 @@
+def solution(genres, plays):
+    import heapq
+    import operator
+
+    answer = []
+    plist = {}    # music list
+    tp = {}    # total play
+    
+    for idx, (genre, play) in enumerate(zip(genres, plays)):
+        tp[genre] = play if not tp.get(genre) else tp[genre] + play
+        
+        if not plist.get(genre):
+            plist[genre] = []
+        
+        heapq.heappush(plist[genre], (-play, idx))
+        
+    tp = sorted(tp.items(), key=operator.itemgetter(1), reverse=True)
+
+    for genre, gc in tp:
+        for _ in range(2):
+            if not plist[genre]:
+                break
+            answer.append(heapq.heappop(plist[genre])[-1])
+    
+    return answer
